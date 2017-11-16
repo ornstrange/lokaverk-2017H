3d78eb8acffa223840e57b5f702e37619d03bf7c
ornst278@nemi.tskoli.is <ornst278@nemi.tskoli.is>
2017-11-16 11:29:59 +0000

    sma sql

--- commands.sql
+++ commands.sql
@@ -1,1 +1,29 @@
+create database 1910952789_fatasida;
+use 1910952789_fatasida;
 
+CREATE TABLE items (
+    iid INT PRIMARY KEY NOT NULL,
+    iname VARCHAR(32) NOT NULL,
+    kind VARCHAR(32),
+    color VARCHAR(16),
+    amount INT,
+    size FLOAT,
+    price INT,
+    arrivalDate DATE,
+    image VARCHAR(100)
+);
+
+CREATE TABLE users (
+    uid INT PRIMARY KEY NOT NULL,
+    username VARCHAR(32) NOT NULL,
+    passwd VARCHAR(32) NOT NULL,
+    email VARCHAR(64) NOT NULL,
+    isAdmin BOOL,
+    fname VARCHAR(32),
+    lname VARCHAR(32)
+);
+
+CREATE TABLE stock (
+    totalPrice INT,
+    amtOfItems INT
+);
