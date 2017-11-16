ab3a69abb76c4b37b6dc8ebdf0f5cfc1d483bd29
ornst278@nemi.tskoli.is <ornst278@nemi.tskoli.is>
2017-11-16 13:15:21 +0000

    sma fikt

---
+++ index.py
@@ -0,0 +1,35 @@
+from bottle import *
+from pymysql import *
+
+connection = connect(host='tsuts.tskoli.is',
+                             user='1910952789',
+                             password='mypassword',
+                             db='1910952789_fatasida',
+                             charset='utf8mb4',
+                             cursorclass=cursors.DictCursor)
+
+@route("/css/main.css")
+def cssmain():
+	return static_file("main.css", "./css")
+
+@route("/css/normalize.css")
+def norm():
+	return static_file("normalize.css", "./css")
+
+@route("/fonts/late-bold.woff2")
+def fontb():
+	return static_file("late-bold.woff2", "./fonts")
+
+@route("/fonts/late-light.woff2")
+def fontl():
+	return static_file("late-light.woff2", "./fonts")
+
+@route("/img/<img:path>")
+def img(img):
+	return static_file(img, "./img")
+
+@route("/")
+def root():
+	return template("index.tpl")
+
+run(host="10.220.221.60", port=8080, debug=True)
---
+++ views/index.tpl
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>LATE</title>
+	<link rel="preload" href="fonts/late-bold.woff2" as="font" type="font/woff2">
+	<link rel="preload" href="fonts/late-light.woff2" as="font" type="font/woff2">
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+	<h1>Gamli</h1>
+	<h1>gegegegemle</h1>
+	<p>gamli gamli</p>
+</body>
+</html>
