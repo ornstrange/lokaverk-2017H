1661ac97f49fcd565d605a8baa3cdd99df137c20
ornstrange <orn.strange@gmail.com>
2017-11-19 22:40:31 +0000

    sunnudagsvinnan

--- index.py
+++ index.py
@@ -1,66 +1,77 @@
 from bottle import *
-from pymysql import *
+from pymysql import connect, cursors
 
-connection = connect(host='tsuts.tskoli.is',
-											user='1910952789',
-											password='mypassword',
-											db='1910952789_fatasida',
-											charset='utf8mb4',
-											cursorclass=cursors.DictCursor)
+CONNECTION = connect(host='tsuts.tskoli.is',
+                     user='1910952789',
+                     password='mypassword',
+                     db='1910952789_fatasida',
+                     charset='utf8mb4',
+                     cursorclass=cursors.DictCursor)
 
-def getData():
-	try:
-		with connection.cursor() as cursor:
-			data = []
-			sql = "SELECT * FROM `users`"
-			cursor.execute(sql)
-			users = cursor.fetchall()
-			data.append(users)
-			sql = "SELECT * FROM `items`"
-			cursor.execute(sql)
-			items = cursor.fetchall()
-			data.append(items)
-			sql = "SELECT * FROM `cart`"
-			cursor.execute(sql)
-			cart = cursor.fetchall()
-			data.append(cart)
-			return data
-	finally:
-		connection.close()
 
-data = getData()
+def get_data():
+    try:
+        with CONNECTION.cursor() as cursor:
+            data = []
+            sql = "SELECT * FROM `users`"
+            cursor.execute(sql)
+            users = cursor.fetchall()
+            data.append(users)
+            sql = "SELECT * FROM `items`"
+            cursor.execute(sql)
+            items = cursor.fetchall()
+            data.append(items)
+            sql = "SELECT * FROM `cart`"
+            cursor.execute(sql)
+            cart = cursor.fetchall()
+            data.append(cart)
+            return data
+    finally:
+        CONNECTION.close()
 
+
+data = get_data()
 print(data)
 
-# leita í data[1] -> data[1][1 - 3]
-# nær í data[1][0] á þeim sem hittir og bætir í lista, og returnar þeim lista
+
 def search_items(string):
-	datalist = []
-	for i in data[1]:
-		if string in [i["iname"], i["kind"], i["color"]]:
-			datalist.append(i["iid"])
-	return datalist
+    datalist = []
+    if string == "+new":
+        for i in data[1]:
+            if i["new"] == "true":
+                datalist.append(i["iid"])
+        return datalist
+    for i in data[1]:
+        if string in [i["iname"], i["kind"], i["color"]]:
+            datalist.append(i["iid"])
+    return datalist
+
 
 @route("/css/main.css")
 def cssmain():
-	return static_file("main.css", "./css")
+    return static_file("main.css", "./css")
+
 
 @route("/css/normalize.css")
-def norm():
-	return static_file("normalize.css", "./css")
+def cssnorm():
+    return static_file("normalize.css", "./css")
+
 
 @route("/img/<img:path>")
 def img(img):
-	return static_file(img, "./img")
+    return static_file(img, "./img")
+
 
 @route("/")
 def root():
-	return template("index.tpl")
+    return template("index.tpl")
+
 
 @route("/search")
-def root():
-	srch = request.query.s
-	items = search_items(srch)
-	return str(items)
+def search():
+    srch = request.query.s
+    items = search_items(srch)
+    return str(items)
+
 
 run(host="localhost", port=8080, debug=True)
--- views/index.tpl
+++ views/index.tpl
@@ -5,14 +5,25 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>LATE</title>
-	<link rel="stylesheet" href="node_modules/normalize.css/normalize.css">
+	<link rel="stylesheet" href="css/normalize.css">
 	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 	<link rel="stylesheet" href="css/main.css">
 </head>
 <body>
 	<header>
 		<div class="items">
-			<button class="menu"><i class="fa fa-bars" aria-hidden="true"></i></button>
+			<button class="menu"><i class="fa fa-bars" aria-hidden="true" id="menu-btn"></i></button>
+			<div class="menu-content" id="menu-cont">
+				<ul>
+					<li class="item"><a href="/search?s=+new">new arrivals</a></li>
+					<li class="item"><a href="/search?s=dress">dresses</a></li>
+					<li class="item"><a href="/search?s=jacket">jackets</a></li>
+					<li class="item"><a href="/search?s=top">tops</a></li>
+					<li class="item filler"></li>
+					<li class="item lower"><a href="#about">about</a></li>
+					<li class="item bottom"><a href="#contact">contact</a></li>
+				</ul>
+			</div>
 			<h1 class="title">LATE</h1>
 			<form action="/search">
 			<input type="text" class="srch-inp" id="srch-inp" name="s">
@@ -32,8 +43,40 @@
 	integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
 	crossorigin="anonymous"></script>
 	<script>
+		var srchClicked = false;
+		var menuClicked = false;
+		$('#menu-btn').click(function(){
+			if (!menuClicked) {
+				$("#menu-cont").addClass("menu-content-active");
+				menuClicked = true;
+			}
+		});
+		$('html').click(function() {
+			if (menuClicked) {
+				$("#menu-cont").removeClass("menu-content-active");
+				menuClicked = false;
+			}
+		});
+		$('#menu-btn').click(function(event){
+			event.stopPropagation();
+		});
+		$('#menu-cont').click(function(event){
+			event.stopPropagation();
+		});
 		$('#srch-btn').click(function(){
-			$("#srch-inp").addClass("srch-inp-active");
+			if (!srchClicked) {
+				srchClicked = true;
+				$("#srch-inp").addClass("srch-inp-active");
+				document.getElementById('srch-inp').focus();
+			} else {
+				srchClicked = false;
+				var srchString = $("#srch-inp").val();
+				if (srchString == "") {
+					$("#srch-inp").removeClass("srch-inp-active");
+				} else {
+					window.location.href = "/search?s="+srchString;
+				}
+			}
 		});
 	</script>
 </body>
